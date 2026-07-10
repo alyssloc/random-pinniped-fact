b1fcc5248e38482f709613f22f0ae135e2bc4664
Alyssa <alyssloc@umich.edu>
2026-07-10 13:44:47 -0400

    adding flask-cors

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 import random, json, os
 
-
-
 app = Flask(__name__)
+CORS(app)
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(BASE_DIR, 'instance', 'pinniped_facts.db')
 
