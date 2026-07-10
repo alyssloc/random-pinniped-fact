6e8a0fb66e141340efd5bd1c13517fbab4fc4fb8
Alyssa <alyssloc@umich.edu>
2026-07-10 15:11:37 -0400

    adding in frontend stuff to this repo

---
+++ frontend/terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "gcp_project_id" {
+  type        = string
+  description = "The unique ID of your Google Cloud Platform project"
+  default     = "your-gcp-project-id" 
+}
+
+variable "gcp_region" {
+  type        = string
+  description = "The target data center region for deployment"
+  default     = "us-central1"
+}
